9a9adde577265b1f9906511a0850f05c3e2eb47e
Crowdbotics <team@crowdbotics.com>
2022-06-24 04:25:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "paycircle_35884",
+  url: "https://paycircle_35884.botics.co",
+  api: "https://paycircle_35884.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
